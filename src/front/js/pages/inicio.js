6be0ee011b54051359ee8190447b8257c75e1059
Ainoha Franco <76441303+Avasidr@users.noreply.github.com>
2024-05-20 19:33:45 +0000

    login fixed home y servicios creados con errores

---
+++ src/front/js/pages/inicio.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Inicio = () => {
+
+  return (
+    <div className={styles.imgfondo} style={{ backgroundImage: "url('src/front/img/entrenador-personal-profesional.jpg')" }}>
+      <div className="text-center mt-5">
+        <h1>MEJORA TU CUERPO AL MÁXIMO</h1>
+        <h2>Los ganadores se ponen METAS, los perdedores EXCUSAS.</h2>
+        <h2>NO PAIN NO GAIN</h2>
+        <img src="src/front/img/image.png" />
+      </div>
+    </div>
+  )
+}
+
+export default Inicio
--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,61 +1,74 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Route, Routes, useNavigate, BrowserRouter, Link } from "react-router-dom";
 import { Context } from "../store/appContext";
+
+import styles from "../../styles/login.module.css";
+
 import { jwtDecode } from 'jwt-decode';
 
 export const Login = () => {
-	const { store, actions } = useContext(Context);
+    const { store, actions } = useContext(Context);
     const navigate = useNavigate();
 
     const [user, setUser] = useState({
-		"email": "",
-		"password": ""
-	})
+        "email": "",
+        "password": ""
+    })
 
     const handleLogin = async () => {
-		await actions.login(user);
+        await actions.login(user);
         const access_token = JSON.parse(localStorage.getItem("token"))
         const access_key = access_token.access_token
         const decodedToken = jwtDecode(access_key);
         const userId = decodedToken.user_id;
         store.user_id = userId
         const userRol = decodedToken.rol;
-        if (userRol===`admin`) {
+        if (userRol === `admin`) {
             navigate(`/admin`);
         } else {
             navigate(`/user`);
-        } 
-	}
+        }
+    }
 
-	const handleSubmit = async (e) => {
-		e.preventDefault()
-		try {
-			await handleLogin()
-		} catch (error) {
-			console.log('error', error)
-		}
-		}
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            await handleLogin()
+        } catch (error) {
+            console.log('error', error)
+        }
+    }
 
-	return (
-		<div className="container">
-			<form onSubmit={handleSubmit}>
-                <div className="mb-3">
-                    <label htmlFor="inputEmail1" className="form-label">Email</label>
-                    <input type="email" 
-                    className="form-control" 
-                    id="inputEmail1" 
-                    aria-describedby="emailHelp" 
-                    onChange={(e) => { setUser({ ...user, email: e.target.value }) }} />
+    return (
+        <div className={styles.imgfondo} style={{ backgroundImage: "url('https://scontent-bcn1-1.xx.fbcdn.net/v/t39.30808-6/326342168_852036582572276_1342480683664754514_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_ohc=qLBF2A6IfTMQ7kNvgFtyMtT&_nc_ht=scontent-bcn1-1.xx&oh=00_AYDH22A6JVJwKiwzaDVoYkrHNrWSajHKleNzw5Zb-zcy2g&oe=664FFB96')" }}>
+            <div className={styles.forms}>
+                <div className={styles.login}>
+                    <h2>Acceso</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className={styles.email}>
+                            <label htmlFor="emailId" className="form-label d-none">Email</label>
+                            <input type="email"
+                                className={`${styles.customInput} `}
+                                id="emailId"
+                                aria-describedby="emailHelp"
+                                onChange={(e) => { setUser({ ...user, email: e.target.value }) }}
+                                placeholder="Email" />
+                        </div>
+                        <div className={styles.password}>
+                            <label htmlFor="passwordId" className="form-label d-none">Password</label>
+                            <input type="password"
+                                className={`${styles.customInput} `}
+                                id="passwordId"
+                                onChange={(e) => { setUser({ ...user, password: e.target.value }) }}
+                                placeholder="Contraseña" />
+                        </div>
+                        <button type="submit" className={styles.button} onClick={handleLogin}>Submit</button>
+                        <Link to="/login">
+                            <div className={styles.noUser}>¿No eres usuario? Registrate</div>
+                        </Link>
+                    </form>
                 </div>
-                <div className="mb-3">
-                    <label htmlFor="inputPassword1" className="form-label">Password</label>
-                    <input type="password" 
-                    className="form-control" 
-                    id="inputPassword1" 
-                    onChange={(e) => { setUser({ ...user, password: e.target.value }) }} />
-                </div>
-                <button type="submit" className="btn btn-primary" onClick={handleLogin}>Submit</button>
-            </form>
-		</div>
-	);
+            </div>
+        </div>
+    );
 };
